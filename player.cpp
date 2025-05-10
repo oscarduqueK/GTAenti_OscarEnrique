60e81a0c27d4498d9572f419ea2fcfae6c43bfc5
Enrique <enrique.giner@enti.cat>
2025-05-10 20:06:48 +0200

    pillar teclas

--- player.cpp
+++ player.cpp
@@ -3,6 +3,24 @@
 
 void Player::move() {
     // Move logic
+    switch (direction)
+    {
+        case '^': // Arriba
+            y--;
+            break;
+        case 'v': // Abajo
+        y++;
+            break;
+        case '<': // Izquierda
+            x--;
+            break;
+        case '>': // Derecha
+            x++;
+            break;
+        default:
+            break;
+    }
+    
 }
 
 void Player::attack() {
