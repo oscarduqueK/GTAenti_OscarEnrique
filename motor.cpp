fa106f68050befac51fe5977dc92d636541096ef
Enrique <enrique.giner@enti.cat>
2025-05-10 20:08:02 +0200

    conexion con teclas y player.h

--- motor.cpp
+++ motor.cpp
@@ -1,9 +1,27 @@
 #include "motor.h"
 #include <windows.h>
+#include "player.h"
 
-void Motor::handleInput() 
+void Motor::handleInput(Player &player)
 {
-    // Input logic
+    if (GetAsyncKeyState(VK_UP)) 
+    {
+        player.direction = '^'; // Mirando hacia arriba
+    } 
+    else if (GetAsyncKeyState(VK_DOWN)) 
+    {
+        player.direction = 'v'; // Mirando hacia abajo
+    } 
+    else if (GetAsyncKeyState(VK_LEFT)) 
+    {
+        player.direction = '<'; // Mirando hacia la izquierda
+    } 
+    else if (GetAsyncKeyState(VK_RIGHT)) 
+    {
+        player.direction = '>'; // Mirando hacia la derecha
+    }
+    
+    player.move(); // Mover a CJ según la dirección actual
 }
 
 void Motor::delay(int milliseconds) 
