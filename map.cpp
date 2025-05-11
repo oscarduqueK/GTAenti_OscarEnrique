40e94a4887bf525381505a7c52da64529f993171
Enrique <enrique.giner@enti.cat>
2025-05-11 11:45:21 +0200

    mapccpp

--- map.cpp
+++ map.cpp
@@ -1,26 +1,59 @@
 #include "map.h"
 #include "player.h"
 #include <iostream>
+#include "utilidad.h"
 
-void Map::display(Player &player)
+void Map::init(int w, int h)
 {
-    
-    //CJ por pantalla
-    
-    for (int i = 0; i < height; i++) 
+    width = w;
+    height = h;
+
+    grid = new char*[height];
+    for (int i = 0; i < height; ++i)
     {
-        for (int j = 0; j < width; j++) 
+        grid[i] = new char[width];
+        for (int j = 0; j < width; ++j)
         {
-            if (i == player.y && j == player.x)
-            {
-                std::cout << player.direction; 
-            } 
-            else 
-            {
-              
-                std::cout << ".";
-            }
+            grid[i][j] = ' '; //empieza como vacio
         }
-        std::cout << std::endl;
     }
+}
+
+
+void Map::display(const Player& player)
+{
+    clearScreen();
+
+    const int viewHeight = 10;
+    const int viewWidth = 20;
+
+    int top = player.y - viewHeight / 2;
+    int left = player.x - viewWidth / 2;
+
+    if (top < 0) top = 0;
+    if (left < 0) left = 0;
+    if (top + viewHeight > height) top = height - viewHeight;
+    if (left + viewWidth > width) left = width - viewWidth;
+
+    for (int i = 0; i < viewHeight; ++i)
+    {
+        for (int j = 0; j < viewWidth; ++j)
+        {
+            int y = top + i;
+            int x = left + j;
+
+            if (y == player.y && x == player.x)
+                std::cout << player.direction;
+            else
+                std::cout << grid[y][x];
+        }
+        std::cout << '\n';
+    }
+}
+
+void Map::cleanup()
+{
+    for (int i = 0; i < height; ++i)
+        delete[] grid[i];
+    delete[] grid;
 }
